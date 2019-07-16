d444958951d1c2639fbc3333142b6f3defa35b34
Bill Glover <bglover@pivotal.io>
2019-07-16 12:36:48 +0100

    Update build output path

--- blue-green/ci/build.sh
+++ blue-green/ci/build.sh
@@ -9,4 +9,5 @@ cd cf-demos/blue-green
 
 echo
 echo "Running build..."
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o artefacts/app . 
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o ../../artefacts/app . 
+cp ci/manifest.yml ../../artefacts/
