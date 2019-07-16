2c99d129d3ce201dbf86c7f8e3eeab63d35944a3
Bill Glover <bglover@pivotal.io>
2019-07-16 15:16:16 +0100

    Set environment variable with deployment colour

--- blue-green/ci/manifest.sh
+++ blue-green/ci/manifest.sh
@@ -2,5 +2,6 @@
 
 set -xe
 
-sed -E "s/(- name: .*)/\1-$(cat ./app-info/next-app.txt)/" cf-demos/blue-green/manifest.yml > artefacts/manifest.yml
+sed -E "s/(- name: .*)/\1-$(cat ./app-info/next-app.txt)/" cf-demos/blue-green/manifest.yml > cf-demos/blue-green/manifest.yml.tmp
+sed -E "s/(.*)unknown(.*)/\1$(cat ./app-info/next-app.txt)\2/" cf-demos/blue-green/manifest.yml.tmp > artefacts/manifest.yml
 cat artefacts/manifest.yml
