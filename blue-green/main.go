9dad3f207982e14af6d0ebdad1a1566995a68d88
Bill Glover <bill@billglover.co.uk>
2019-07-16 21:53:22 +0100

    Respond with JSON body

--- blue-green/main.go
+++ blue-green/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 )
+
+// Payload is the response returned by the server. It simply indicates whether
+// this is a blue or a green deployment.
+type Payload struct {
+	Color string `json:"color,omitempty"`
+}
 
 var color string
 
@@ -15,5 +22,8 @@ func main() {
 
 // Hello handles HTTP requests and returns the value of the color variable.
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Active deployment: " + color + "\n"))
+	p := Payload{Color: color}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(p)
+	w.WriteHeader(http.StatusOK)
 }
