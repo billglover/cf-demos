4a98760922cb1333aaf51e4ab0a806b2f336dbcf
Stephen Midgley <smidgley@pivotal.io>
2019-09-25 17:09:40 +0100

    Amazing change

--- blue-green/main.go
+++ blue-green/main.go
@@ -19,5 +19,5 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Deployment-Colour", colour)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello, %s", colour)
+	fmt.Fprintf(w, "Hello, %s!", colour)
 }
